271952f8c1440007c3345876bf3844cb4f2b56c6
Dan <dan@dc-home.ch>
2011-03-09 19:35:51 +0100

    Created basic symfony2 bundle

---
+++ DreamcraftWordCloudBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Dreamcraft\WordCloudBundle
+ *
+ * Copyright (c) 2010-2011 dreamcraft.ch
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Dreamcraft\WordCloudBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DreamcraftWordCloudBundle extends Bundle
+{
+}
